3f4e33248954868f60b9bbd2334e092b287485c1
Pathorn <pathornsomsaen@gmail.com>
2025-10-04 03:31:16 +0700

    rebase code

---
+++ ViewModels/DetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿// /ViewModels/PostDetailsViewModel.cs
+namespace Hackathon.ViewModels
+{
+    public class PostDetailsViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? RequiredExpertise { get; set; }
+        public DateTime? ExpirationDate { get; set; }
+        public bool IsClosed { get; set; }
+        public string? AuthorName { get; set; }
+        public int MaxParticipants { get; set; }
+    }
+}
